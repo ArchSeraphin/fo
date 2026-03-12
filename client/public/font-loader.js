50aa9f6a6ee0673c46d3d5808d94487ed94fcc49
ArchSeraphin <trinity.informatique@gmail.com>
2026-03-12 15:59:55 +0100

    v3.5.2
    Fix score lighthouse

---
+++ client/public/font-loader.js
@@ -0,0 +1,6 @@
+(function () {
+  var l = document.createElement('link');
+  l.rel = 'stylesheet';
+  l.href = 'https://fonts.googleapis.com/css2?family=Fraunces:ital,opsz,wght@0,9..144,400;0,9..144,600;0,9..144,700;1,9..144,400&family=Source+Sans+3:wght@300;400;500;600&display=swap';
+  document.head.appendChild(l);
+}());
